2589c391bb159d2faf490629bd111cec7e93bdbe
Vinicius Kenji dos Santos Enoki <viniciuskenji@PREFEITURA.SP.GOV.BR>
2026-02-19 17:06:34 -0300

    feat: instalação e configuração de libs - lucide e svgrrc

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              dimensions: false, // Remove width/height fixos do SVG
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: "removeViewBox",
+                    active: false, // Mantém o viewBox
+                  },
+                ],
+              },
+            },
+          },
+        ],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
